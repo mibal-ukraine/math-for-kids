08e49469eecf4afacaff8cbc720032cee1f179cd
mibal_ua <9mohapx9@gmail.com>
2022-05-28 12:50:32 +0200

    Add init version

---
+++ app/src/main/java/com/mibalstudio/mathforkids/MainActivity.java
@@ -0,0 +1,44 @@
+package com.mibalstudio.mathforkids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+
+    CheckBox examplesCheckBox;
+
+    CheckBox comparisonsCheckBox;
+
+    CheckBox tasksCheckBox;
+
+    Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        examplesCheckBox = findViewById(R.id.examplesCheckBox);
+        comparisonsCheckBox = findViewById(R.id.comparisonsCheckBox);
+        tasksCheckBox = findViewById(R.id.tasksCheckBox);
+
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(v -> {
+            /*
+            boolean checked = ((CheckBox) startButton).isChecked();
+
+
+            Intent intent = new Intent(class);
+
+             */
+        });
+    }
+
+
+}
